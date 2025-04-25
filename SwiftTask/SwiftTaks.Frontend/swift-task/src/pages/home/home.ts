40329cdc566fd3dbd3eaf8a9d83136feef0ec926
Kenan Isakovic <kenaniskvc@gmx.net>
2025-04-25 20:15:18 +0200

    Created Project Structure for multi page project

---
+++ SwiftTask/SwiftTaks.Frontend/swift-task/src/pages/home/home.ts
@@ -0,0 +1,19 @@
+// const apiurl = 'https://localhost:7050/';
+
+// fetch(apiurl + 'api/Task')
+
+// async function getTask() {
+//     const url = apiurl + "api/Task";
+//     try {
+//       const response = await fetch(url);
+//       if (!response.ok) {
+//         throw new Error(`Response status: ${response.status}`);
+//       }
+  
+//       const json = await response.json();
+
+//       console.log(json);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
