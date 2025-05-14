306528d75e99ed4fdad03e396e19d3db9ef5f90f
KenIskvc <kenan.sosic@gmx.net>
2025-05-14 18:40:15 +0200

    Code Clean Up, Fixed Home.html

--- SwiftTask/SwiftTask.Backend/Controllers/TopicController.cs
+++ SwiftTask/SwiftTask.Backend/Controllers/TopicController.cs
@@ -1,125 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SwiftTask.Backend.DTOs;
 using SwiftTask.Backend.Infrastructure;
 using SwiftTask.Backend.Models;
 
-namespace SwiftTask.Backend.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TopicController : ControllerBase
-    {
-        private readonly SwiftTaskDbContext _context;
+namespace SwiftTask.Backend.Controllers;
 
-        public TopicController(SwiftTaskDbContext context)
-        {
-            _context = context;
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class TopicController : ControllerBase {
+    private readonly SwiftTaskDbContext _context;
 
-        // GET: api/Topic
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<TopicDto>>> GetTopics(string userId)
-        {
-            //var topics = await _context.Topics.WInclude(t => t.Tasks).ToListAsync();
-            var topics = await _context.Topics.Where(t => t.SwiftTaskUserId.Equals(userId))
-                .Include(t => t.Tasks).ToListAsync();
-                
+    public TopicController(SwiftTaskDbContext context) => _context = context;
 
-            var topicDtos = topics.Select(topic => new TopicDto
-            {
-                Id = topic.Id,
-                Name = topic.Name,
-                Tasks = topic.Tasks.Select(task => new TaskDto
-                {
-                    Id = task.Id,
-                    Description = task.Description
-                }).ToList()
-            }).ToList();
+    // GET: api/Topic
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TopicDto>>> GetTopics(string userId) {
+        //var topics = await _context.Topics.WInclude(t => t.Tasks).ToListAsync();
+        var topics = await _context.Topics.Where(t => t.SwiftTaskUserId.Equals(userId))
+            .Include(t => t.Tasks).ToListAsync();
 
-            return topicDtos;
-        }
 
-        // GET: api/Topic/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Topic>> GetTopic(int id)
-        {
-            var topic = await _context.Topics.FindAsync(id);
+        var topicDtos = topics.Select(topic => new TopicDto {
+            Id = topic.Id,
+            Name = topic.Name,
+            Tasks = topic.Tasks.Select(task => new TaskDto {
+                Id = task.Id,
+                Description = task.Description
+            }).ToList()
+        }).ToList();
 
-            if (topic == null)
-            {
-                return NotFound();
-            }
-
-            return topic;
-        }
-
-        // PUT: api/Topic/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTopic(int id, Topic topic)
-        {
-            if (id != topic.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(topic).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TopicExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Topic
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Topic>> PostTopic(Topic topic)
-        {
-            _context.Topics.Add(topic);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTopic", new { id = topic.Id }, topic);
-        }
-
-        // DELETE: api/Topic/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTopic(int id)
-        {
-            var topic = await _context.Topics.FindAsync(id);
-            if (topic == null)
-            {
-                return NotFound();
-            }
-
-            _context.Topics.Remove(topic);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool TopicExists(int id)
-        {
-            return _context.Topics.Any(e => e.Id == id);
-        }
+        return topicDtos;
     }
+
+    // GET: api/Topic/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Topic>> GetTopic(int id) {
+        var topic = await _context.Topics.FindAsync(id);
+
+        return topic == null ? (ActionResult<Topic>) NotFound() : (ActionResult<Topic>) topic;
+    }
+
+    // PUT: api/Topic/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTopic(int id, Topic topic) {
+        if(id != topic.Id) {
+            return BadRequest();
+        }
+
+        _context.Entry(topic).State = EntityState.Modified;
+
+        try {
+            await _context.SaveChangesAsync();
+        } catch(DbUpdateConcurrencyException) {
+            if(!TopicExists(id)) {
+                return NotFound();
+            } else {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Topic
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Topic>> PostTopic(Topic topic) {
+        _context.Topics.Add(topic);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetTopic", new { id = topic.Id }, topic);
+    }
+
+    // DELETE: api/Topic/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTopic(int id) {
+        var topic = await _context.Topics.FindAsync(id);
+        if(topic == null) {
+            return NotFound();
+        }
+
+        _context.Topics.Remove(topic);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool TopicExists(int id) => _context.Topics.Any(e => e.Id == id);
 }
