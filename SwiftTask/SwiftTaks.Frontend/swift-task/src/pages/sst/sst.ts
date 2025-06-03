43f7095dde09eb6f5e12996384cdcabb43b4aa17
KenIskvc <kenan.sosic@gmx.net>
2025-06-03 14:57:38 +0200

    Added display of speech to text message.

--- SwiftTask/SwiftTaks.Frontend/swift-task/src/pages/sst/sst.ts
+++ SwiftTask/SwiftTaks.Frontend/swift-task/src/pages/sst/sst.ts
@@ -1,7 +1,12 @@
 import SstService from "../../services/stt-service";
 
 document.getElementById('recordBtn')?.addEventListener('click', () => {
-  SstService.startRecognition(async (text) => {
+  SstService.startRecognition(async (text: string) => {
     console.log("Recognized:", text);
+    
+    const outputElement = document.getElementById("output");
+    if (outputElement) {
+      outputElement.innerHTML = text;
+    }
   });
 });
