327401238d58322aab23bc6480065502bad8136f
Kenan Isakovic <kenaniskvc@gmx.net>
2025-06-09 15:08:35 +0200

    Extended Layout.ts

--- SwiftTask/SwiftTaks.Frontend/swift-task/src/components/layout/layout.ts
+++ SwiftTask/SwiftTaks.Frontend/swift-task/src/components/layout/layout.ts
@@ -2,11 +2,18 @@ export function mountLayout(): void {
   const app = document.getElementById("app")!;
   app.innerHTML = `
     <div id="layout" class="layout">
-      <header>
-        <nav>SwiftTask - <a href="/home" data-navigo>Home</a> | <a href="/profile" data-navigo>Profile</a></nav>
-      </header>
+      <aside class="sidebar">
+            <h1>SwiftTask</h1>
+            <nav>
+                <ul>
+                  <li><a href="/home" data-navigo>Home</a></li>
+                  <li><a href="/dashboard" data-navigo>Dashboard</a></li>
+                  <li><a href="/tasks" data-navigo>Tasks</a></li>
+                  <li><a href="/topics" data-navigo>Topics</a></li>
+                </ul>
+            </nav>
+        </aside>
       <main id="app-content">
-        <!-- Page content will go here -->
       </main>
     </div>
   `;
